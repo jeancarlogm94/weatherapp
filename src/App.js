31e0e3e1360f48a56f4dd880264d55800224a0a9
John Doe <johndoe@example.com>
2022-06-20 14:26:11 -0500

    change style

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ import getFormattedWeatherData from './services/weatherService';
 import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Loading from './components/Loading';
 
 function App() {
   const [query, setQuery] = useState({ q: 'bogota' });
   const [units, setUnits] = useState('metric');
   const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchWeather = async () => {
       const message = query.q ? query.q : 'current location.';
-
+      setLoading(true);
       toast.info('Fetching weather for ' + message);
 
       await getFormattedWeatherData({ ...query, units }).then((data) => {
@@ -26,6 +28,7 @@ function App() {
         );
 
         setWeather(data);
+        setLoading(false);
       });
     };
 
@@ -33,32 +36,40 @@ function App() {
   }, [query, units]);
 
   const formatBackground = () => {
-    if (!weather) return 'from-cyan-600 to-blue-500';
+    if (!weather) return 'from-cyan-500 to-blue-400';
     const threshold = units === 'metric' ? 27 : 80;
-    if (weather.temp <= threshold) return 'from-cyan-600 to-blue-500';
+    if (weather.temp <= threshold) return 'from-cyan-500 to-blue-400';
 
-    return 'from-yellow-600 to-orange-500';
+    return 'from-yellow-500 to-orange-500';
   };
 
   return (
     <div>
       <div
-        className={`mx-auto max-w-md my-4 pt-5 pb-1 px-8 bg-gradient-to-br  h-fit shadow-md shadow-gray-400 ${formatBackground()} rounded-lg`}
+        className={`mx-auto max-w-md my-4 pt-5 pb-1 px-8 bg-gradient-to-br from-cyan-500 to-blue-400 h-fit shadow-md shadow-gray-400 rounded-lg`}
       >
+        <p className="text-white text-center text-4xl font-medium mb-3">
+          Weather App
+        </p>
+        <img src="" alt="" />
         <TopButtons setQuery={setQuery} />
         <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
       </div>
 
-      {weather && (
-        <div
-          className={`mx-auto max-w-md my-4 py-6 px-8 bg-gradient-to-br  h-fit shadow-md shadow-gray-400 ${formatBackground()} rounded-lg`}
-        >
-          <TimeAndLocation weather={weather} />
-          <TemperatureAndDetails weather={weather} />
+      {loading ? (
+        <Loading />
+      ) : (
+        weather && (
+          <div
+            className={`mx-auto max-w-md my-4 py-6 px-8 bg-gradient-to-br  h-fit shadow-md shadow-gray-400 ${formatBackground()} rounded-lg`}
+          >
+            <TimeAndLocation weather={weather} />
+            <TemperatureAndDetails weather={weather} />
 
-          <Forecast title="hourly forecast" items={weather.hourly} />
-          <Forecast title="daily forecast" items={weather.daily} />
-        </div>
+            <Forecast title="hourly forecast" items={weather.hourly} />
+            <Forecast title="daily forecast" items={weather.daily} />
+          </div>
+        )
       )}
 
       <ToastContainer autoClose={1000} theme="colored" newestOnTop={true} />
