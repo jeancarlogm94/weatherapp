8115ee46f20f0e068b20831e91438d13644c05b5
John Doe <johndoe@example.com>
2022-06-19 21:08:52 -0500

    first commit

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -54,7 +54,7 @@ function TemperatureAndDetails({
       <div className="flex flex-row items-center justify-center my-3 space-x-2 text-white text-sm">
         <UilSun />
         <p className="font-light">
-          Rise:{" "}
+          Rise
           <span className="font-medium ml-1">
             {formatToLocalTime(sunrise, timezone, "hh:mma")}
           </span>
@@ -63,7 +63,7 @@ function TemperatureAndDetails({
 
         <UilSunset />
         <p className="font-light">
-          Set:{" "}
+          Set
           <span className="font-medium ml-1">
             {formatToLocalTime(sunset, timezone, "hh:mma")}
           </span>
@@ -72,14 +72,14 @@ function TemperatureAndDetails({
 
         <UilSun />
         <p className="font-light">
-          High:{" "}
+          High
           <span className="font-medium ml-1">{`${temp_max.toFixed()}°`}</span>
         </p>
         <p className="font-light">|</p>
 
         <UilSun />
         <p className="font-light">
-          Low:{" "}
+          Low
           <span className="font-medium ml-1">{`${temp_min.toFixed()}°`}</span>
         </p>
       </div>
