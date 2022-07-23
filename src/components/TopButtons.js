df13cb62146cad718e0d243317043567e391b9ec
John Doe <johndoe@example.com>
2022-07-23 17:53:10 -0500

    add map location

--- src/components/TopButtons.js
+++ src/components/TopButtons.js
@@ -19,21 +19,17 @@ function TopButtons({ setQuery }) {
       id: 4,
       title: 'Tokyo',
     },
-    {
-      id: 5,
-      title: 'Toronto',
-    },
   ];
 
   return (
     <div>
-      <Row className="d-flex justify-content-center">
+      <Row className="d-flex justify-content-center mb-3">
         <Col md={10}>
           <div className="d-flex justify-content-center">
             {cities.map((city) => (
               <button
                 key={city.id}
-                className="mx-auto text-white text-xl font-medium transition ease-out hover:scale-125"
+                className="mx-auto text-white text-2xl font-medium transition ease-out hover:scale-125"
                 onClick={() => setQuery({ q: city.title })}
               >
                 {city.title}
